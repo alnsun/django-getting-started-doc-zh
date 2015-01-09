ea0385d48f9490e3e525d445f2b8a163f4e14ac0
alnsun <alnsun.cn@gmail.com>
2015-01-09 23:55:12 +0800

    update

---
+++ build/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:46,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to Django Getting Started&#8217;s documentation!"],objects:{},titleterms:{welcom:0,get:0,indic:0,django:0,start:0,tabl:0,document:0}})
